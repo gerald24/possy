273a1bad1b60c941ee5ab2582d1d04700c9e4ff2
Alex Gassner <hello@agsolutions.at>
2019-11-17 17:26:18 +0100

    add index test

---
+++ daemon/src/test/kotlin/net/g24/possy/daemon/IndexControllerTest.kt
@@ -0,0 +1,54 @@
+/*
+ * This file is part of possy.
+ *
+ * possy is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * possy is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with possy. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.g24.possy.daemon
+
+import org.hamcrest.Matchers.containsString
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mockito.`when`
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.info.GitProperties
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.Instant
+
+@WebMvcTest(IndexController::class)
+@ExtendWith(SpringExtension::class)
+class IndexControllerTest {
+
+    @MockBean
+    private lateinit var gitProperties: GitProperties
+
+    @Autowired
+    private lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `index shows version info`() {
+        `when`(gitProperties.get("commit.id.describe")).thenReturn("1.0.0")
+        `when`(gitProperties.getInstant("build.time")).thenReturn(Instant.now())
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk)
+                .andExpect(content().string(containsString(gitProperties.get("commit.id.describe"))))
+    }
+}
